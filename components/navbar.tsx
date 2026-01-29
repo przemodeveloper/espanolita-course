8eab0fb185334e6b1612157fbcc00094b0eb3777
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2026-01-28 20:01:53 -0500

    Add active link styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,13 +6,19 @@ import logo from "@/public/logo.png";
 import { Button } from "@/components/ui/button";
 import type { User } from "@supabase/supabase-js";
 import { useLogout } from "@/queries/useLogout";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 export function Navbar({ user }: { user: User | null }) {
   const isAuthenticated = user?.aud === "authenticated";
   const { mutate: logout } = useLogout();
-
+  const pathname = usePathname();
   const initials =
     user?.user_metadata?.first_name?.[0] + user?.user_metadata?.last_name?.[0];
+
+  const isLinkActive = (path: string) => {
+    return pathname === path;
+  };
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 flex flex-wrap justify-between items-center p-4 bg-white border-b">
@@ -33,17 +39,40 @@ export function Navbar({ user }: { user: User | null }) {
         {!isAuthenticated && (
           <>
             <li>
-              <Link href="/login">Logowanie</Link>
+              <Link
+                href="/login"
+                className={clsx(
+                  isLinkActive("/login") &&
+                    "text-primary underline decoration-red-500",
+                )}
+              >
+                Logowanie
+              </Link>
             </li>
             <li>
-              <Link href="/register">Rejestracja</Link>
+              <Link
+                href="/register"
+                className={clsx(
+                  isLinkActive("/register") &&
+                    "text-primary underline decoration-red-500",
+                )}
+              >
+                Rejestracja
+              </Link>
             </li>
           </>
         )}
         {isAuthenticated && (
           <li>
             <Link href="/course" className="flex items-center">
-              Panel użytkownika{" "}
+              <span
+                className={clsx(
+                  isLinkActive("/course") &&
+                    "text-primary text-primary underline decoration-red-500",
+                )}
+              >
+                Panel użytkownika
+              </span>{" "}
               <span className="text-xs ml-2 bg-gray-200 rounded-full w-6 h-6 flex items-center justify-center">
                 {initials}
               </span>
@@ -52,7 +81,15 @@ export function Navbar({ user }: { user: User | null }) {
         )}
         {!isAuthenticated && (
           <li>
-            <Link href="/checkout">Zakup kursu</Link>
+            <Link
+              href="/checkout"
+              className={clsx(
+                isLinkActive("/checkout") &&
+                  "text-primary underline decoration-red-500",
+              )}
+            >
+              Zakup kursu
+            </Link>
           </li>
         )}
         {isAuthenticated && (
