a6ba9ea26776cac220cacf1a9d8e359e9d13014f
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2026-03-24 17:31:51 -0500

    Delete padding on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-64px)] w-full py-12">
+    <div className="flex items-center justify-center min-h-[calc(100vh-64px)] w-full">
       <LoginForm className="w-full max-w-md" />
     </div>
   );
