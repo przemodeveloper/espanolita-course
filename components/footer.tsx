9b9726c4fd9eac5e2db78e17e884e427f94fac18
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2026-04-24 07:22:08 +0200

    Add info in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,6 +6,12 @@ export function Footer() {
           &copy; {new Date().getFullYear()} Szkoła językowa Españolita. Wszelkie
           prawa zastrzeżone.
         </p>
+        <p className="text-center text-gray-500 text-sm">
+          Developed by{" "}
+          <a href="https://pgdeveloper.net/" className="text-blue-500">
+            Przemysław Górecki
+          </a>
+        </p>
       </div>
     </footer>
   );
