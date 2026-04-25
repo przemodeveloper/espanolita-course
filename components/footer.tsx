75109d6d8f4b1e4bf42ba7adb481430d4b0ccdc0
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2026-04-25 11:09:01 +0200

    Split conditions and privacy links

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,6 +8,22 @@ export function Footer() {
           &copy; {new Date().getFullYear()} Szkoła językowa Españolita. Wszelkie
           prawa zastrzeżone.
         </p>
+        <div className="flex justify-center gap-4">
+          <Link
+            target="_blank"
+            href="https://coffee-hayley-30.tiiny.site/regulamin-kursu.pdf"
+            className="text-sm text-blue-500 underline"
+          >
+            Regulamin
+          </Link>
+          <Link
+            target="_blank"
+            href="https://coffee-hayley-30.tiiny.site/polityka-prywatności.pdf"
+            className="text-sm text-blue-500 underline"
+          >
+            Polityka prywatności
+          </Link>
+        </div>
         <p className="text-center text-gray-500 text-sm">
           Developed by{" "}
           <Link
