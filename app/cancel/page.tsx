b044e2f879991bcfc7c49108832fd38c2f4c6c9b
przemodeveloper <33373656+przemodeveloper@users.noreply.github.com>
2026-04-28 19:59:22 +0200

    Change terms acceptance flow

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,17 +1,25 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Płatność nie powiodła się - Kurs maturalny Españolita",
+  title: "Płatność nie powiodła się - Matura Españolita",
   description: "Płatność nie powiodła się.",
   keywords: [
     "kurs maturalny",
     "hiszpański",
     "matura",
+    "espanolita",
     "españolita",
     "płatność",
   ],
 };
 
 export default function CancelPage() {
-  return <h1>Płatność zakończona niepowodzeniem 💔</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-semibold">Płatność nie powiodła się 💔</h1>
+      <p className="text-lg text-muted-foreground mb-4">
+        Płatność nie powiodła się. Spróbuj ponownie.
+      </p>
+    </div>
+  );
 }
